ff650b16728cc542fca357bb07bf9f62f5e22ad5
marciolucasjunior <109992150+marciolucasjunior@users.noreply.github.com>
2023-03-13 12:31:26 -0300

    overflow

--- Hamburger/src/Components/Pedidos.jsx
+++ Hamburger/src/Components/Pedidos.jsx
@@ -46,50 +46,50 @@ function Pedidos() {
 
 
  {usuarios.length > 0 ?
-    <table>
-    <thead>
-       <tr>
-       
-           <td>Cliente</td>
-           <td>pão</td>
-           <td>Carne</td>
-           <td>Opcionais</td>
-           <td>Ações</td>
-       </tr>
-    </thead>
-    {usuarios.map(usuarios =>(
-    <tbody key={usuarios._id.toString()}>
+    <div id="container_tabela">
+      <table>
+      <thead>
          <tr>
-          <td>{usuarios.cliente}</td>
-          <td>{usuarios.pao}</td>
-          <td>{usuarios.carne}</td>
-         <td>
-            <ul>
-              <li> {usuarios.bacon}</li>
-              <li> {usuarios.salame}</li>
-              <li> {usuarios.cebola}</li>
-              <li> {usuarios.chedar}</li>
-              <li> {usuarios.pepino}</li>
-              <li>{usuarios.tomate}</li>
-            </ul>
-          
-          </td>
-          
-          
-          <td id='btn'>
-          <select name="" id="finalizado">
-                <option value="Solicitado">Solicitado</option>
-                <option value="Em_producao">Em produção</option>
-                <option value="Finalizado">Finalizado</option>
-            </select>
-           <button onClick={()=> deletar(usuarios._id)}>cancelar</button>
-          </td>
+      
+             <td>Cliente</td>
+             <td>pão</td>
+             <td>Carne</td>
+             <td>Opcionais</td>
+             <td>Ações</td>
          </tr>
-       </tbody>
-     ))}  
-
-       </table>
-   
+      </thead>
+      {usuarios.map(usuarios =>(
+      <tbody key={usuarios._id.toString()}>
+           <tr>
+            <td>{usuarios.cliente}</td>
+            <td>{usuarios.pao}</td>
+            <td>{usuarios.carne}</td>
+           <td>
+              <ul>
+                <li> {usuarios.bacon}</li>
+                <li> {usuarios.salame}</li>
+                <li> {usuarios.cebola}</li>
+                <li> {usuarios.chedar}</li>
+                <li> {usuarios.pepino}</li>
+                <li>{usuarios.tomate}</li>
+              </ul>
+      
+            </td>
+      
+      
+            <td id='btn'>
+            <select name="" id="finalizado">
+                  <option value="Solicitado">Solicitado</option>
+                  <option value="Em_producao">Em produção</option>
+                  <option value="Finalizado">Finalizado</option>
+              </select>
+             <button onClick={()=> deletar(usuarios._id)}>cancelar</button>
+            </td>
+           </tr>
+         </tbody>
+       ))}
+         </table>
+    </div>
   
 
 :<h2>Aguardando Pedidos...</h2>}
